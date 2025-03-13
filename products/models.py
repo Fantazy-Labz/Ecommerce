67435b5693484d8f34e08fbe0a73368b396f9992
salog0d <david.salomon.nava11@gmail.com>
2025-03-13 12:14:35 -0600

    Added product details and dashboard functionalities also added default image for product and some boostrap dor a more interactive design and user experience in development stage

--- products/models.py
+++ products/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
     price = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100000)])
     category = models.CharField(max_length=30, choices = OPTIONS)
     stock = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    image = models.ImageField(upload_to='product_pic', default='default.png')
 
     def __str__(self):
         return self.name
